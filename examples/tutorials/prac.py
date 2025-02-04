428849fde681b0302a1dc0509d85254a40c6e755
Yuhan Jin <yuhan.jin@dfki.de>
2025-02-04 20:24:59 +0100

    up

--- examples/tutorials/prac.py
+++ examples/tutorials/prac.py
@@ -1,11 +1,10 @@
 import genesis as gs
-import mujoco as mj
 gs.init(backend=gs.gpu)
 
 scene = gs.Scene(
   show_viewer=False,
   viewer_options=gs.options.ViewerOptions(
-    res=(640, 480),
+    res=(3840, 2160),
     max_FPS=30,
     camera_fov = 40,
   ),
@@ -17,31 +16,41 @@ scene = gs.Scene(
 )
 
 plane = scene.add_entity(
-    morph=gs.morphs.Plane(),
+  morph=gs.morphs.Plane()
 )
 
 cam = scene.add_camera(
-  res= (640, 480),
+  res= (3840, 2160),
   pos=(0, 10, 10),
   lookat=(0, 0, 0),
   fov=40,
   GUI=False,
 )
 
+# man = scene.add_entity(
+#   gs.morphs.MJCF(file = "/workspace/genesis/assets/xml/mujoco/model/hammock/humanoid_body.xml",
+#                  pos = (0, 0, 1),),
+#   visualize_contact=True,
+# )
+
 arter = scene.add_entity(
   gs.morphs.URDF(file = "/workspace/genesis/assets/urdf/arter/arter.urdf",
-                 pos = (0, 0, 1),),
+                 pos = (0, 0, 2),
+                 ),
   visualize_contact=True,
 )
- 
+
+cylinder = scene.add_entity(
+  gs.morphs.Cylinder(radius=0.2, height=2, pos=(0, 2, 1)),
+)
+
 scene.build()
 
 cam.start_recording()
 
 for i in range(1000):
   scene.step()
-  cam.render()
-  rgb, depth, seg, normal = cam.render(depth=True, segmentation=True, normal=True)
-  
+  cam.render(depth=True)
+  # rgb, depth, seg, normal = cam.render(depth=True, segmentation=False, normal=False)
 
 cam.stop_recording(save_to_filename="arter.mp4", fps=60)
